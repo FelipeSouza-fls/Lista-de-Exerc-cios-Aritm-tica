583ed01f8d0dcb442f03fd611c4f54b12974dd30
FelipeSouza-fls <166164209+FelipeSouza-fls@users.noreply.github.com>
2024-04-15 20:09:54 -0300

    Add files via upload

---
+++ Lista1ex12/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1ex12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double val1;
+            double val2;
+            double val3;
+            double val4;
+            double val5;
+            double pago;
+            double result;
+
+            Console.Write("Vlor do 1º produto: ");
+            val1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Vlor do 2º produto: ");
+            val2 = double.Parse(Console.ReadLine());
+
+            Console.Write("Vlor do 3º produto: ");
+            val3 = double.Parse(Console.ReadLine());
+
+            Console.Write("Vlor do 4º produto: ");
+            val4 = double.Parse(Console.ReadLine());
+
+            Console.Write("Vlor do 5º produto: ");
+            val5 = double.Parse(Console.ReadLine());
+
+            Console.Write("Vlor pago: ");
+            pago = double.Parse(Console.ReadLine());
+
+            result = pago - (val1 + val2 + val3 + val4 + val5);
+            Console.Write("O troco é: ");
+            Console.WriteLine(result);
+        }
+    }
+}
